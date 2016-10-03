a2ef0f84268db91c921b05a56e0e2b45d24d16b4
Katsutami7moto <nclsf87@gmail.com>
2016-10-03 20:36:38 +0300

    The script is working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 3_bars
+
+Enter the path to a .json, then enter your longitude and latitude to find the closest bar.
+Hit enter to quit the script.
--- bars.py
+++ bars.py
@@ -6,7 +6,7 @@ def load_data(filepath: str) -> list:
     if not os.path.exists(filepath):
         return None
     with open(filepath, encoding='utf-8') as handle:
-        return json.JSONDecoder().decode(handle)
+        return json.load(handle)
 
 
 def name(bar: dict) -> str:
@@ -17,8 +17,8 @@ def seats(bar: dict) -> int:
     return bar['Cells']['SeatsCount']
 
 
-def coordinates(bar: dict) -> list:
-    return bar['Cells']['goeData']['coordinates']
+def coordinates(bar: dict) -> tuple:
+    return bar['Cells']['geoData']['coordinates'][0], bar['Cells']['geoData']['coordinates'][1]
 
 
 def get_biggest_bar(data: list) -> list:
@@ -38,7 +38,7 @@ def get_smallest_bar(data: list) -> list:
     min_seats = 9 * 10**10
     result = []
     for bar in data:
-        if seats(bar) < min_seats:
+        if seats(bar) < min_seats and not (seats(bar) == 0):
             min_seats = seats(bar)
             result.clear()
             result.append(name(bar))
@@ -47,18 +47,18 @@ def get_smallest_bar(data: list) -> list:
     return result
 
 
-def get_closest_bar(data: list, longitude: float, latitude: float) -> list:
-    min_distance = 9 * 10**10
+def get_closest_bar(data: list, longitude: str, latitude: str) -> list:
+    min_distance = 9.0 * 10**10
     result = []
 
-    def find_distance(x1, x2, y1, y2):
+    def find_distance(x1, y1, x2, y2):
         from math import sqrt
         return sqrt((x2-x1)**2 + (y2-y1)**2)
 
     for bar in data:
         bar_coordinates = coordinates(bar)
-        distance = find_distance(bar_coordinates[0], bar_coordinates[1], longitude, latitude)
-        if distance < min_distance:
+        distance = find_distance(bar_coordinates[0], bar_coordinates[1], float(longitude), float(latitude))
+        if distance < min_distance and not (distance == 0):
             min_distance = distance
             result.clear()
             result.append(name(bar))
@@ -68,4 +68,21 @@ def get_closest_bar(data: list, longitude: float, latitude: float) -> list:
 
 
 if __name__ == '__main__':
-    pass
+    path = input('Input a path to your JSON file: ')
+    if not path:
+        print("File not found.\n")
+    else:
+        data_to_use = load_data(path)
+        if not data_to_use:
+            print("It's not a JSON file.\n")
+        else:
+            print("The biggest bar(-s): {}\n".format(get_biggest_bar(data_to_use)))
+            print("The smallest bar(-s): {}\n".format(get_smallest_bar(data_to_use)))
+            while True:
+                your_longitude = input("Input your longitude: ")
+                if not your_longitude:
+                    break
+                your_latitude = input("Input your latitude: ")
+                if not your_latitude:
+                    break
+                print("The closest bar(-s): {}\n".format(get_closest_bar(data_to_use, your_longitude, your_latitude)))
