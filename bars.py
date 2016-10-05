64444be8e0ae94de9a60989ee998a42192d1f763
Katsutami7moto <nclsf87@gmail.com>
2016-10-05 22:58:32 +0300

    Refactored "if __name__ == '__main__:'"

--- bars.py
+++ bars.py
@@ -1,7 +1,7 @@
 import json
 import os
-
-CANT_BE_MORE = 9.0 * 10**9
+from math import sqrt
+import sys
 
 
 def load_data(filepath: str) -> list:
@@ -23,49 +23,41 @@ def coordinates(bar: dict) -> tuple:
     return bar['Cells']['geoData']['coordinates'][0], bar['Cells']['geoData']['coordinates'][1]
 
 
-def get_biggest_bar(data: list) -> list:
+def get_biggest_bar(data: list) -> str:
     result = max(data, key=seats)
     return name(result)
 
 
-def get_smallest_bar(data: list) -> list:
+def get_smallest_bar(data: list) -> str:
     result = min(data, key=seats)
     return name(result)
 
 
-def get_closest_bar(data: list, longitude: str, latitude: str) -> list:
-    min_distance = CANT_BE_MORE
-    result = []
+def get_closest_bar(data: list, longitude: str, latitude: str) -> str:
 
-    def find_distance(x1, y1, x2, y2):
-        from math import sqrt
-        return sqrt((x2-x1)**2 + (y2-y1)**2)
+    def find_distance(bar: dict) -> float:
+        x1, y1 = coordinates(bar)
+        return sqrt((float(longitude)-x1)**2 + (float(latitude)-y1)**2)
 
-    for bar in data:
-        bar_coordinates = coordinates(bar)
-        distance = find_distance(bar_coordinates[0], bar_coordinates[1], float(longitude), float(latitude))
-        if distance < min_distance:
-            min_distance = distance
-            result.clear()
-            result.append(name(bar))
-        elif distance == min_distance:
-            result.append(name(bar))
-    return result
+    result = min(data, key=find_distance)
+    return name(result)
 
 
 if __name__ == '__main__':
     path = input('Input a path to your JSON file: ')
     data_to_use = load_data(path)
     if not data_to_use:
-        print("File not found.\n")
-    else:
-        print("The biggest bar(-s): {}\n".format(get_biggest_bar(data_to_use)))
-        print("The smallest bar(-s): {}\n".format(get_smallest_bar(data_to_use)))
-        while True:
-            your_longitude = input("Input your longitude: ")
-            if not your_longitude:
-                break
-            your_latitude = input("Input your latitude: ")
-            if not your_latitude:
-                break
-            print("The closest bar(-s): {}\n".format(get_closest_bar(data_to_use, your_longitude, your_latitude)))
+        print('File not found.\n')
+        sys.exit(1)
+
+    print('The biggest bar(-s): "{}"\n'.format(get_biggest_bar(data_to_use)))
+    print('The smallest bar(-s): "{}"\n'.format(get_smallest_bar(data_to_use)))
+
+    while True:
+        your_longitude = input('Input your longitude: ')
+        if not your_longitude:
+            break
+        your_latitude = input('Input your latitude: ')
+        if not your_latitude:
+            break
+        print('The closest bar(-s): "{}"\n'.format(get_closest_bar(data_to_use, your_longitude, your_latitude)))
